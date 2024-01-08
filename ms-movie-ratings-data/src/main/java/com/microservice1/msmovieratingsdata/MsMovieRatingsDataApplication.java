cf087fda0381643710625314027d0a744e5e8629
Vignashwar Veeramani <vignashwarv@gmail.com>
2024-01-08 15:27:25 +1100

    3. Initial Commit - ms-movie-ratings-data

---
+++ ms-movie-ratings-data/src/main/java/com/microservice1/msmovieratingsdata/MsMovieRatingsDataApplication.java
@@ -0,0 +1,13 @@
+package com.microservice1.msmovieratingsdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMovieRatingsDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMovieRatingsDataApplication.class, args);
+	}
+
+}
