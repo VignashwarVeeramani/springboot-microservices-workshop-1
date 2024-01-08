3210fef2dc20be327a325e1ca5c5022bc378682d
Vignashwar Veeramani <vignashwarv@gmail.com>
2024-01-08 15:25:48 +1100

    2. Initial Commit - ms-movie-catalog

---
+++ ms-movie-catalog/src/main/java/com/microservice1/msmoviecatalog/MsMovieCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.microservice1.msmoviecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMovieCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMovieCatalogApplication.class, args);
+	}
+
+}
---
+++ ms-movie-catalog/src/test/java/com/microservice1/msmoviecatalog/MsMovieCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice1.msmoviecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMovieCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
