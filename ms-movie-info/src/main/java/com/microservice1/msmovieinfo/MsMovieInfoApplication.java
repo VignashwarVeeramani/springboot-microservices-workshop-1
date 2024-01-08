1ee04bf89be54811f433fa17dfa073e60a53b884
Vignashwar Veeramani <vignashwarv@gmail.com>
2024-01-08 15:16:19 +1100

    1.Initial commit - ms-movie-info

---
+++ ms-movie-info/src/main/java/com/microservice1/msmovieinfo/MsMovieInfoApplication.java
@@ -0,0 +1,13 @@
+package com.microservice1.msmovieinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMovieInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMovieInfoApplication.class, args);
+	}
+
+}
---
+++ ms-movie-info/src/test/java/com/microservice1/msmovieinfo/MsMovieInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice1.msmovieinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMovieInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
