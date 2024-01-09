88b6122fd6bf51d68cffac151eac6e9185e58298
Vignashwar Veeramani <vignashwarv@gmail.com>
2024-01-09 11:51:49 +1100

    5.Create Movie info Controller, model classes and update MovieCatalog service server port

--- ms-movie-catalog/src/main/java/com/microservice1/msmoviecatalog/controller/MovieCatalogController.java
+++ ms-movie-catalog/src/main/java/com/microservice1/msmoviecatalog/controller/MovieCatalogController.java
@@ -1,2 +1,22 @@
-package com.microservice1.msmoviecatalog.controller;public class MovieCatalogController {
+package com.microservice1.msmoviecatalog.controller;
+
+import com.microservice1.msmoviecatalog.models.CatalogItem;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogController {
+
+    @GetMapping("/{userId}")
+    public List<CatalogItem> getCatalogItem(@PathVariable("userId") String userId) {
+        return Collections.singletonList(
+                new CatalogItem("Avatar", "Awesome Movie", 8)
+        );
+    }
 }
--- ms-movie-catalog/src/main/java/com/microservice1/msmoviecatalog/models/CatalogItem.java
+++ ms-movie-catalog/src/main/java/com/microservice1/msmoviecatalog/models/CatalogItem.java
@@ -1,2 +1,39 @@
-package com.microservice1.msmoviecatalog.models;public class CatalogItem {
+package com.microservice1.msmoviecatalog.models;
+
+
+public class CatalogItem {
+
+    private String name;
+    private String desc;
+    private int rating;
+
+    public CatalogItem(String name, String desc, int rating) {
+        this.name = name;
+        this.desc = desc;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
 }
